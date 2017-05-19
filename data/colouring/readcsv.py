d5eb40324e813f07722749303c46bfbeb98580d4
Pratik Shastri <hmpratik@gmail.com>
2017-05-19 15:14:47 +0000

    Update readcsv.py

--- data/colouring/readcsv.py
+++ data/colouring/readcsv.py
@@ -10,12 +10,19 @@ with open('Toread.csv', 'rb') as csvfile:
     #print test
 n = 85 #total_cuisines = 85
 cuisines_graph = [[0 for x in range(n)] for y in range(n)]
+cuisine_occurence = [0 for x in range(n)]
 for rest in test.keys():
     for i in range(0,n):
-        for j in range(0,i):
-            if str(i) in test[rest] and str(j) in test[rest]:
-                if len(test[rest]) == 2: cuisines_graph[i][j] += 1.0
-                else: cuisines_graph[i][j] += 2.0/(len(test[rest])*(len(test[rest])-1))
+        if str(i) in test[rest]:
+            cuisine_occurence += 1
+    for i in range(0,n):
+        for j in range(0,n):
+            if str(i) in test[rest] and str(j) in test[rest] and i != j:
+                cuisines_graph[i][j] += 1
+for i in range(0,n):
+    for j in range(0,n):
+        cuisines_graph[i][j] /= cuisine_occurence[i] 
+
 with open('result2.csv', 'w') as csvfile:
 	csvfileWriter = csv.writer(csvfile)
 	for i in range(0,n):
